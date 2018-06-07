d27949942cf8f3941554b6aba4f66ae89d899c49
吕江腾 <lvjiangteng@126.com>
2018-06-07 16:28:59 +0800

    子传父用回调

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react'
 
 class CommentInput extends Component {
-  render() {
+  constructor(){
+    super()
+    this.state={
+      username: '',
+      content: ''
+    }
+  }
+  handleInputChange=e=>{
+    this.setState({
+      username:e.target.value
+    })
+  }
+  handleTextAreaChange=e=>{
+    this.setState({
+      content:e.target.value
+    })
+  }
+  handleSubmit=()=>{
+    if(this.props.onSubmit){
+      const { username, content } = this.state
+      this.props.onSubmit({username, content})
+    }
+    this.setState({ content: '' })
+  }
+
+  render () {
     return (
-      <div>CommentInput</div>
+      <div className='comment-input'>
+        <div className='comment-field'>
+          <span className='comment-field-name'>用户名：</span>
+          <div className='comment-field-input'>
+            <input onChange={this.handleInputChange} value={this.state.value}/>
+          </div>
+        </div>
+        <div className='comment-field'>
+          <span className='comment-field-name'>评论内容：</span>
+          <div className='comment-field-input'>
+            <textarea onChange={this.handleTextAreaChange} value={this.state.content}/>
+          </div>
+        </div>
+        <div className='comment-field-button'>
+          <button onClick={this.handleSubmit}>
+            发布
+          </button>
+        </div>
+      </div>
     )
   }
 }
