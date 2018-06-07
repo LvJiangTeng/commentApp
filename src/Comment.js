1a2f9d24839e53c8f9ba79c00f03176c32f8da6f
吕江腾 <lvjiangteng@126.com>
2018-06-07 16:12:14 +0800

    结构形成

--- src/Comment.js
+++ src/Comment.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-// import CommentApp from './containers/CommentApp'
-import CommentApp from './CommentApp'
+import CommentApp from './containers/CommentApp'
 import commentsReducer from './reducers/comments'
 import DatePicker from 'antd/lib/date-picker'
 import 'antd/dist/antd.css';
@@ -12,12 +11,14 @@ import './index.css'
 
 const store = createStore(commentsReducer)
 
-ReactDOM.render(
-    <CommentApp />,
-  document.getElementById('root')
-);
 // ReactDOM.render(
-//   <DatePicker >
-//   </DatePicker>,
+//   <Provider store={store}>
+//     <CommentApp />
+//   </Provider>,
 //   document.getElementById('root')
 // );
+ReactDOM.render(
+  <DatePicker >
+  </DatePicker>,
+  document.getElementById('root')
+);
