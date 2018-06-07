b876c5333950330e3f5dd174369e007ced225ff5
吕江腾 <lvjiangteng@126.com>
2018-06-07 16:05:15 +0800

    lalal

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import CommentApp from './containers/CommentApp'
+import commentsReducer from './reducers/comments'
+import DatePicker from 'antd/lib/date-picker'
+import 'antd/dist/antd.less';
+
+import './index.css'
+
+const store = createStore(commentsReducer)
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <CommentApp />
+//   </Provider>,
+//   document.getElementById('root')
+// );
+ReactDOM.render(
+  <DatePicker >
+  </DatePicker>,
+  document.getElementById('root')
+);
