cfca1daafecf8dc96804a8afdd9db2cd1375d225
吕江腾 <lvjiangteng@126.com>
2018-06-08 13:50:31 +0800

    高阶组件的使用

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react'
 import CommentInput from './CommentInput'
 import CommentList from './CommentList'
-
+import PropTypes from 'prop-types';
+import wrapWithLoadData from './wrapWithLoadData'
 class CommentApp extends Component {
-  constructor(){
-    super()
+  static propTypes = {
+    data: PropTypes.any,
+    saveData: PropTypes.func.isRequired
+  }
+  constructor(props){
+    super(props)
     this.state={
-      comments:[]
+      comments: props.data
     }
   }
-  componentWillMount(){
-    this._loadComments()
-  }
-  _saveComments=(comments)=>{
-    localStorage.setItem('comments',JSON.stringify(comments))
-  }
-  _loadComments=()=>{
-    let comments=localStorage.getItem('comments');
-    if(comments) {
-      comments = JSON.parse(comments)
-      this.setState({comments})}
-  }
+  
   handleSubmitComment=comment=>{
     if (!comment) return
     if (!comment.username) return alert('请输入用户名')
@@ -30,13 +24,13 @@ class CommentApp extends Component {
     this.setState({
       comments
     })
-    this._saveComments(comments)
+    this.props.saveData(comments)
   }
   handleDeleteComment =(index)=> {
     const comments=this.state.comments
     comments.splice(index, 1)
     this.setState({comments})
-    this._saveComments(comments)
+    this.props.saveData(comments)
   }
   render() {
     return (
@@ -47,5 +41,5 @@ class CommentApp extends Component {
     )
   }
 }
-
+CommentApp = wrapWithLoadData(CommentApp, 'comments')
 export default CommentApp
--- src/index.js
+++ src/index.js
@@ -7,15 +7,30 @@ import CommentApp from './CommentApp'
 import commentsReducer from './reducers/comments'
 import DatePicker from 'antd/lib/date-picker'
 import 'antd/dist/antd.css';
+import InputWithUserName from './InputWithUserName'
+
 
 import './index.css'
 
 const store = createStore(commentsReducer)
 
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        用户名：<InputWithUserName />
+      </div>
+    )
+  }
+}
 ReactDOM.render(
-    <CommentApp />,
-  document.getElementById('root')
+  <CommentApp />,
+document.getElementById('root')
 );
+// ReactDOM.render(
+//   <Index />,
+// document.getElementById('root')
+// );
 // ReactDOM.render(
 //   <DatePicker >
 //   </DatePicker>,
