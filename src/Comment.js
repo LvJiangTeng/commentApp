4ce70bd46ae33c83f94f322a74034c2efdd6e04c
吕江腾 <lvjiangteng@126.com>
2018-06-08 10:41:59 +0800

    删除评论

--- src/Comment.js
+++ src/Comment.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 class Comment extends Component {
   static propTypes = {
-    comment: PropTypes.object.isRequired
+    comment: PropTypes.object.isRequired,
+    onDeleteComment: PropTypes.func,
+    index: PropTypes.number
   }
   constructor () {
     super()
@@ -16,6 +18,9 @@ class Comment extends Component {
       5000
     )
   }
+  componentWillUnmount () {
+    clearInterval(this._timer)
+  }
   _updateTimeString =()=> {
     const comment = this.props.comment
     const duration = (+Date.now() - comment.createdTime) / 1000
@@ -25,15 +30,37 @@ class Comment extends Component {
         : `${Math.round(Math.max(duration, 1))} 秒前`
     })
   }
+  _getProcessedContent= (content) =>{
+    return content
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;")
+    .replace(/`([\S\s]+?)`/g, '<code>$1</code>')
+  }
+  handleDeleteComment=()=>{
+    if (this.props.onDeleteComment) {
+      this.props.onDeleteComment(this.props.index)
+    }
+  }
   render () {
+    const comment=this.props.comment
     return (
       <div className='comment'>
         <div className='comment-user'>
-          <span>{this.props.comment.username} </span>：
+          <span>{comment.username} </span>：
         </div>
-        <p>{this.props.comment.content}</p>
+        <p dangerouslySetInnerHTML={{
+          __html: this._getProcessedContent(comment.content)
+        }} />
         <span className='comment-createdtime'>
           {this.state.timeString}
+        </span>
+        <span 
+          onClick={this.handleDeleteComment}
+        className='comment-delete'>
+        删除
         </span>
       </div>
     )
--- src/CommentList.js
+++ src/CommentList.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
 import Comment from './Comment'
+import PropTypes from 'prop-types';
 class CommentList extends Component {
+  static propTypes = {
+    comments: PropTypes.array,
+    onDeleteComment: PropTypes.func
+  }
   static defaultProps = {
     comments: []
   }
   constructor(props){
     super(props)
   }
-  
+  handleDeleteComment=(index)=>{
+    if (this.props.onDeleteComment) {
+      this.props.onDeleteComment(index)
+    }
+  }
   render() {
     const comments=this.props.comments
     return (
       <div>{comments.map((comment, i) => {
         return (
-          <Comment comment={comment} key={i}></Comment>
+          <Comment comment={comment} key={i} index={i} onDeleteComment={this.handleDeleteComment}></Comment>
         )
       })}</div>
     )
