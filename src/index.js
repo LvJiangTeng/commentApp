ee66cf07fb04852af41baaba48c4c4c39cae7f49
吕江腾 <lvjiangteng@126.com>
2018-06-26 17:24:53 +0800

    完成

--- src/index.js
+++ src/index.js
@@ -2,37 +2,16 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-// import CommentApp from './containers/CommentApp'
-import CommentApp from './CommentApp'
+import CommentApp from './containers/CommentApp'
 import commentsReducer from './reducers/comments'
-import DatePicker from 'antd/lib/date-picker'
-import 'antd/dist/antd.css';
-import InputWithUserName from './InputWithUserName'
-
-
 import './index.css'
 
 const store = createStore(commentsReducer)
 
-class Index extends Component {
-  render () {
-    return (
-      <div>
-        用户名：<InputWithUserName />
-      </div>
-    )
-  }
-}
+
 ReactDOM.render(
-  <CommentApp />,
-document.getElementById('root')
+  <Provider store={store}>
+    <CommentApp />
+  </Provider>,
+  document.getElementById('root')
 );
-// ReactDOM.render(
-//   <Index />,
-// document.getElementById('root')
-// );
-// ReactDOM.render(
-//   <DatePicker >
-//   </DatePicker>,
-//   document.getElementById('root')
-// );
